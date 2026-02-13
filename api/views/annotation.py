95e6415b3e3b46b3ad6310fb653da10d09fce328
shremus <shreyash.musale@noc.ac.uk>
2026-02-13 17:12:16 +0000

    Removed unwanted import

--- api/views/annotation.py
+++ api/views/annotation.py
@@ -7,7 +7,6 @@ from rest_framework.parsers import FormParser, MultiPartParser
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.status import HTTP_201_CREATED, HTTP_400_BAD_REQUEST
-from rest_framework.views import APIView
 
 from api.models import Annotation, AnnotationLabel, Annotator
 from api.serializers import AnnotationLabelSerializer, AnnotationSerializer, AnnotatorSerializer, FileUploadSerializer
