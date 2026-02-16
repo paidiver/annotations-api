cc20547634b3be6c38933ef1c941ad3ad17d8252
shremus <shreyash.musale@noc.ac.uk>
2026-02-16 10:47:40 +0000

    extract logic modified

--- api/views/annotation.py
+++ api/views/annotation.py
@@ -12,63 +12,24 @@ from api.models import Annotation, AnnotationLabel, Annotator
 from api.serializers import AnnotationLabelSerializer, AnnotationSerializer, AnnotatorSerializer, FileUploadSerializer
 
 EVENT_HEADER_KEYS = {
-    "image-set-name",
-    "image-project",
-    "image-context",
-    "image-abstract",
-    "image-event",
-    "image-platform",
-    "image-sensor",
-    "image-set-uuid",
-    "image-set-handle",
-    "image-set-ifdo-version",
-    "image-creators",
-    "image-pi",
-    "image-license",
-    "image-copyright",
-    "image-coordinate-reference-system",
-    "image-coordinate-uncertainty-meters",
-    "image-longitude",
-    "image-latitude",
-    "image-altitude-meters",
-    "image-acquisition",
-    "image-quality",
-    "image-deployment",
-    "image-navigation",
-    "image-scale-reference",
-    "image-illumination",
-    "image-pixel-magnitude",
-    "image-marine-zone",
-    "image-spectral-resolution",
-    "image-capture-mode",
-    "image-area-square-meter",
-    "image-meters-above-ground",
-    "image-acquisition-settings",
-    "image-set-local-path",
-    "image-datetime-format",
-    "image-camera-housing-viewport",
-    "image-flatport-parameters",
-    "image-domeport-parameters",
-    "image-camera-calibration-model",
-    "image-photometric-calibration",
-    "image-objective",
-    "image-target-environment",
-    "image-target-timescale",
-    "image-spatial-constraints",
-    "image-temporal-constraints",
-    "image-time-synchronisation",
-    "image-item-identification-scheme",
-    "image-curation-protocol",
-    "image-visual-constraints",
-    "image-set-min-latitude-degrees",
-    "image-set-max-latitude-degrees",
-    "image-set-min-longitude-degrees",
-    "image-set-max-longitude-degrees",
-    "image-set-start-datetime",
-    "image-set-end-datetime",
-    "image-set-related-material",
-    "image-set-provenance",
-    "image-fauna-attraction",
+    "annotation-set-name",
+    "annotation-project",
+    "annotation-context",
+    "annotation-abstract",
+    "annotation-objective",
+    "annotation-target-environment",
+    "annotation-target-timescale",
+    "annotation-curation-protocol",
+    "annotation-creators",
+    "annotation-pi",
+    "annotation-license",
+    "annotation-copyright",
+    "annotation-set-uuid",
+    "annotation-set-handle",
+    "annotation-set-version",
+    "annotation-image-set-name",
+    "annotation-image-set-uuid",
+    "annotation-image-set-handle,"
 }
 
 
@@ -126,30 +87,34 @@ class UploadAnnotationsView(viewsets.ViewSet):
             )
 
         try:
-            df = pd.read_excel(file, sheet_name="Event header")
+            df = pd.read_excel(file, sheet_name="Annotation set metadata")
         except Exception:
             return Response(
                 {"error": "Failed to read Excel file."},
                 status=HTTP_400_BAD_REQUEST,
             )
 
-        # Iterate through each row to store values in a dict.
-        event_header = {}
-        for _, row in df.iterrows():
-            if row.empty or pd.isna(row.iloc[0]):
+        print(df)
+        df = df.iloc[:, :3]
+        annotation_data = {}
+        for main_key, sub_key, value in df.itertuples(index=False, name=None):
+            if pd.notna(main_key):
+                current_main_key = str(main_key).strip()
+
+            if not current_main_key:
                 continue
 
-            key = str(row.iloc[0]).strip()
-            value = row.iloc[1]
+            final_key = f"{current_main_key}-{str(sub_key).strip()}" if pd.notna(sub_key) else current_main_key
 
-            if key in EVENT_HEADER_KEYS and pd.notna(value):
-                event_header[key] = value
 
-        print("Parsed Event header:")
-        for k, v in event_header.items():
+            if pd.notna(value):
+                annotation_data[final_key] = value
+
+        print("Parsed Annotation set metadata:")
+        for k, v in annotation_data.items():
             print(f"{k}: {v}")
 
         return Response(
-            {"status": "uploaded"},
+            {"status": "uploaded", "data": annotation_data},
             status=HTTP_201_CREATED,
         )
