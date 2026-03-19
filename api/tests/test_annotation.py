1bfdf8e78a7843caaaa9f0e65bcaf15cdb214619
shremus <shreyash.musale@noc.ac.uk>
2026-03-19 12:48:24 +0000

    missing file assertion error fixed

--- api/tests/test_annotation.py
+++ api/tests/test_annotation.py
@@ -244,4 +244,4 @@ class UploadAnnotationsViewTests(APITestCase):
         )
 
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertEqual(response.data["file"][0], "This field is required.")
+        self.assertEqual(response.data["file"][0], "No file was submitted.")
