07c416e01df94e1b172de1862eb60d174bb5279e
tobias ferreira <soutobias@yahoo.com.br>
2026-06-17 16:56:41 +0100

    Update migration ob

--- api/migrations/0001_initial.py
+++ api/migrations/0001_initial.py
@@ -17,10 +17,6 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RunSQL(
-            sql="CREATE EXTENSION IF NOT EXISTS postgis",
-            reverse_sql="DROP EXTENSION IF EXISTS postgis",
-        ),
         migrations.CreateModel(
             name='Annotation',
             fields=[
