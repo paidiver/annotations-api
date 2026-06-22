2b2042b39bbf3cd6a1719bb8acc7f3f3d007684f
tobias ferreira <soutobias@yahoo.com.br>
2026-06-22 12:34:25 +0100

    Add local files for swagger ui

--- config/settings.py
+++ config/settings.py
@@ -33,6 +33,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "rest_framework.authtoken",
     "drf_spectacular",
+    "drf_spectacular_sidecar",
     "api",
 ]
 
@@ -55,6 +56,8 @@ SPECTACULAR_SETTINGS = {
     "DESCRIPTION": "API for managing annotations and related metadata for images captured in various contexts.",
     "VERSION": "0.1.0",
     "COMPONENT_SPLIT_REQUEST": True,
+    "SWAGGER_UI_DIST": "SIDECAR",
+    "SWAGGER_UI_FAVICON_HREF": "SIDECAR",
     "TAGS": [
         {"name": "Images API", "description": "Endpoints for managing images and image sets"},
         {"name": "Annotations API", "description": "Endpoints for managing annotations"},
@@ -71,6 +74,7 @@ SPECTACULAR_SETTINGS = {
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -162,3 +166,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
